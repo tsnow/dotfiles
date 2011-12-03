fadf9adcb723acced2badfaa197a644d349f5d68
Tim Snowhite <tim@snowhitesolutions.com>
2011-12-03 16:50:46 -0500

    missing elpa files

---
+++ emacs-tsnow/.emacs.d/elpa/changelog-url-0.1/changelog-url-autoloads.el
@@ -0,0 +1,28 @@
+;;; changelog-url-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (changelog-url-buttonize) "changelog-url" "changelog-url.el"
+;;;;;;  (20048 8135))
+;;; Generated autoloads from changelog-url.el
+
+(autoload 'changelog-url-buttonize "changelog-url" "\
+Not documented
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("changelog-url-pkg.el") (20048 8135 363400))
+
+;;;***
+
+(provide 'changelog-url-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; changelog-url-autoloads.el ends here
