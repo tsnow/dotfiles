b7642c9f8d2a272e0f4705ac9eab86d2233e542b
Tim Snowhite <tim@snowhitesolutions.com>
2011-12-03 16:06:36 -0500

    VQN's .emacs, my erlang changes and brew's emacs changes

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,3 +1,4 @@
 alias e="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -n"
 alias git=hub
 alias rake="rake -qs"
+alias emacs="/usr/local/Cellar/emacs/HEAD/Emacs.app/Contents/MacOS/Emacs -nw --no-init"
