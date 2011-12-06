5d319b375a7a41a43206b6cc8191ad39ef9c166f
Tim Snowhite <tsnowhite@taximagic.com>
2011-12-05 20:31:39 -0500

    things

--- emacs-tsnow/tsnow.el
+++ emacs-tsnow/tsnow.el
@@ -68,6 +68,7 @@
 
 ;;;; END el-get
 
+
 (put 'erase-buffer 'disabled nil)
 
 (global-set-key (kbd "C-x f") 'find-file-at-point)
@@ -209,7 +210,35 @@
    (add-to-list 'load-path "/usr/local/Cellar/erlang/R14B04/lib/erlang/lib/tools-2.6.6.5/emacs")
    (setq erlang-root-dir "/usr/local/Cellar/erlang/R14B04/lib/erlang")
    (add-to-list 'exec-path "/usr/local/Cellar/erlang/R14B04/lib/erlang/bin")
-   (require 'erlang-start)))
+   (require 'erlang-start)
+
+
+   ;;; from http://alexott.net/en/writings/emacs-devenv/EmacsErlang.html
+   (defun my-erlang-mode-hook ()
+        ;; when starting an Erlang shell in Emacs, default in the node name
+        (setq inferior-erlang-machine-options '("-sname" "emacs"))
+        ;; add Erlang functions to an imenu menu
+        (imenu-add-to-menubar "imenu")
+        ;; customize keys
+        (local-set-key [return] 'newline-and-indent)
+        )
+   ;; Some Erlang customizations
+   (add-hook 'erlang-mode-hook 'my-erlang-mode-hook)
+
+   (defun flymake-erlang-init ()
+     (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                        'flymake-create-temp-inplace))
+            (local-file (file-relative-name
+                         temp-file
+                         (file-name-directory buffer-file-name))))
+       (list "~/my-dotfiles/erlang/eflymake" (list local-file))))
+   (add-to-list 'flymake-allowed-file-name-masks
+                '("\\.erl\\'" flymake-erlang-init))
+   (add-hook 'find-file-hook 'flymake-find-file-hook)
+   (defun my-erlang-mode-hook ()
+        (flymake-mode 1))
+(add-hook 'erlang-mode-hook 'my-erlang-mode-hook)
+   ))
 
 
 (setenv "PAGER" "/bin/cat")
--- install.sh
+++ install.sh
@@ -1,20 +1,25 @@
 git submodule update --init
 
-. ~/my/dotfiles/staging/install.sh
-ln -s ~/my/dotfiles/bash/main.sh ~/.profile
+. ~/my-dotfiles/staging/install.sh
+ln -s ~/my-dotfiles/bash/main.sh ~/.profile
 ln -s ~/.profile ~/.bash_profile
 ln -s ~/.profile ~/.bashrc
 
-ln -s ~/my/dotfiles/emacs ~/.emacs.d
-ln -s ~/my/dotfiles/emacs-tsnow/tsnow.el ~/.emacs.d/tsnow.el
-ln -s ~/my/dotfiles/emacs-tsnow/tsnow ~/.emacs.d/tsnow
+ln -s ~/my-dotfiles/emacs ~/.emacs.d
+ln -s ~/my-dotfiles/emacs-tsnow ~/emacs-tsnow
 
-ln -s ~/my/dotfiles/gitconfig ~/.gitconfig
-#ln -s ~/my/dotfiles/hoe/hoerc ~/.hoerc
-#ln -s ~/my/dotfiles/hoe/template ~/.hoe_template
-ln -s ~/my/dotfiles/zshrc ~/.zshrc
-ln -s ~/my/dotfiles/oh-my-zsh ~/.oh-my-zsh
+ln -s ~/my-dotfiles/gitconfig ~/.gitconfig
+#ln -s ~/my-dotfiles/hoe/hoerc ~/.hoerc
+#ln -s ~/my-dotfiles/hoe/template ~/.hoe_template
+ln -s ~/my-dotfiles/zshrc ~/.zshrc
+ln -s ~/my-dotfiles/oh-my-zsh ~/.oh-my-zsh
 
 
 . ~/.profile
 
+if [ -e ~/dev-dotfiles/install.sh ];
+    . ~/dev-dotfiles/install.sh
+fi
+if [ -e ~/site-dotfiles/install.sh ];
+    . ~/site-dotfiles/install.sh
+fi
