29790b5fa275131964ba40be800a4ee606016d82
Tim Snowhite <tsnowhite@taximagic.com>
2012-01-02 16:28:29 -0500

    Moving between windows the osx way.

--- emacs-tsnow/tsnow.el
+++ emacs-tsnow/tsnow.el
@@ -240,7 +240,18 @@
         (flymake-mode 1))
 (add-hook 'erlang-mode-hook 'my-erlang-mode-hook)
    ))
+(defun select-next-window ()
+  "Switch to the next window"
+  (interactive)
+  (select-window (next-window)))
 
+(defun select-previous-window ()
+  "Switch to the previous window"
+  (interactive)
+  (select-window (previous-window)))
+
+(global-set-key (kbd "M-<right>") 'select-next-window)
+(global-set-key (kbd "M-<left>")  'select-previous-window)
 
 (setenv "PAGER" "/bin/cat")
 (server-start)
