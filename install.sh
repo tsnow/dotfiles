5b7590c04e914d20da6e0af44d69db9743faea2f
John Barnette <jbarnette@gmail.com>
2012-04-20 01:05:33 -0400

    use adamv's relink style; NEW recurse into subdirectories and dev-dotfiles and site-dotfiles alike, searching for install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,50 @@
+#!/bin/bash
+
+# from https://github.com/adamv/dotfiles/commit/54f571c2cf52a8eabd21043602f3bbfca430fd84 #adamv's dotfiles
+# relink target-file source-file
+relink() {
+  if [[ -h "$1" ]]; then
+    echo "Relinking $1"
+    # Symbolic link? Then recreate.
+    rm "$1"
+    ln -sn "$2" "$1"
+  elif [[ ! -e "$1" ]]; then
+    echo "Linking $1 to $2"
+    ln -sn "$2" "$1"
+  else
+    echo "$1 exists as a real file, skipping."
+  fi
+}
+
+DIR=$( cd "$( dirname "$0" )" && pwd )
+
+# relink ~/.bash_profile $DIR/bash_profile
+# relink ~/.bashrc       $DIR/bashrc
+
+
 git submodule update --init
 
-. ~/my-dotfiles/staging/install.sh
-ln -s ~/my-dotfiles/bash/main.sh ~/.profile
-ln -s ~/.profile ~/.bash_profile
-ln -s ~/.profile ~/.bashrc
+relink  ~/.profile $DIR/bash/main.sh
 
-ln -s ~/my-dotfiles/emacs ~/.emacs.d
-ln -s ~/my-dotfiles/emacs-tsnow ~/emacs-tsnow
+relink ~/.bash_profile ~/.profile 
+relink ~/.bashrc ~/.profile 
 
-ln -s ~/my-dotfiles/gitconfig ~/.gitconfig
-#ln -s ~/my-dotfiles/hoe/hoerc ~/.hoerc
-#ln -s ~/my-dotfiles/hoe/template ~/.hoe_template
-ln -s ~/my-dotfiles/zshrc ~/.zshrc
-ln -s ~/my-dotfiles/oh-my-zsh ~/.oh-my-zsh
+relink ~/.gitconfig    $DIR/gitconfig
 
+relink ~/.emacs.d $DIR/emacs 
+relink ~/emacs-tsnow $DIR/emacs-tsnow 
+
+#relink ~/.hoerc $DIR/hoe/hoerc 
+#relink ~/.hoe_template $DIR/hoe/template 
+
+relink  ~/.oh-my-zsh $DIR/oh-my-zsh
+relink ~/.zshrc $DIR/zshrc
 
 . ~/.profile
 
-if [ -e ~/dev-dotfiles/install.sh ];
-    . ~/dev-dotfiles/install.sh
-fi
-if [ -e ~/site-dotfiles/install.sh ];
-    . ~/site-dotfiles/install.sh
-fi
+
+# Recurse into subdirectories
+for i in `find $DIR/staging ~/dev-dotfiles ~/site-dotfiles -type f -name 'install.sh' 2>/dev/null`; do 
+    echo "Running: $i";
+    source $i;
+done
