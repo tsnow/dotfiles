02cbd221fd38b521f4703bb2a1cf38eb7269964a
Tim Snowhite <tsnowhite@taximagic.com>
2012-12-11 10:50:24 -0500

    destroying my terminal sessions for some reason

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -2,22 +2,3 @@ alias e="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -n"
 #alias git=hub
 alias rake="rake -qs"
 
-# Bash snippet to open new shells in most recently visited dir.
-# Useful if you want to open a new terminal tab at the present 
-# tab's location.
-#
-# Put this in your .bashrc or whatever. 
-pathed_cd () {
-    if [ "$1" == "" ]; then
-        cd
-    else
-        cd "$1"
-    fi
-#	echo "Going to $1 from "`cat ~/.cdpath`
-    pwd > ~/.cdpath
-}
-alias cd="pathed_cd"
- 
-if [ -f ~/.cdpath ]; then
-  cd $(cat ~/.cdpath)
-fi
