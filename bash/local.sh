021fc2a7891a2c986c7705a74d03b89bb542f3e7
Tim Snowhite <snowhitster@gmail.com>
2015-06-02 21:24:27 -0700

    nothing

--- bash/local.sh
+++ bash/local.sh
@@ -1,3 +1,3 @@
-alias up='ruby -rubygems ~/gitup.rb fetch --username snowhitster@gmail.com --password random'
+
 export PATH="/usr/local/bin:$PATH"
 #export PATH="/Applications/Postgres.app/Contents/MacOS/bin:$PATH"
